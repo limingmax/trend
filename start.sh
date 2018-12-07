d53c8d81eb920a0966be4eb3275855d40886fa28
limingmax <limingmax@163.com>
2018-12-07 16:55:35 +0800

    test

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 set -x
 
 sed -i "s/KAFKA_IP/${KAFKA_IP}/g" python.ini
-sed -i "s/KAFKA_PORT/= ${KAFKA_PORT}/g" python.ini
-sed -i "s/CONSUMER_TOPIC/= ${CONSUMER_TOPIC}/g" python.ini
-sed -i "s/PRODUCER_TOPIC/= ${PRODUCER_TOPIC}/g" python.ini
+sed -i "s/KAFKA_PORT/${KAFKA_PORT}/g" python.ini
+sed -i "s/CONSUMER_TOPIC/${CONSUMER_TOPIC}/g" python.ini
+sed -i "s/PRODUCER_TOPIC/${PRODUCER_TOPIC}/g" python.ini
 
-sed -i "s/HBASE_IP/= ${HBASE_IP}/g" python.ini
-sed -i "s/HBASE_PORT/= ${HBASE_PORT}/g" python.ini
+sed -i "s/HBASE_IP/${HBASE_IP}/g" python.ini
+sed -i "s/HBASE_PORT/${HBASE_PORT}/g" python.ini
 
 sed -i "s/REDIS_IP:REDIS_PORT/${REDIS_IP}:${REDIS_PORT}/g" python.ini
 
